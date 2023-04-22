23b33c176ff9264b47ae49fd9b98ceaa92ab3a41
Ron <ron@lisles.net>
2023-04-22 07:01:41 -0500

    Initial Commit

---
+++ PatriotRV/PatriotRVApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatriotRVApp.swift
+//  PatriotRV
+//
+//  Created by Ron Lisle on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct PatriotRVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
