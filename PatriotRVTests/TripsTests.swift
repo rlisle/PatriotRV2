a1d6702e79629977139eb206c8050d7236e739c5
Ron <ron@lisles.net>
2023-04-22 10:49:36 -0500

    Add Trips.next tests

--- PatriotRVTests/TripsTests.swift
+++ PatriotRVTests/TripsTests.swift
@@ -25,4 +25,35 @@ final class TripsTests: XCTestCase {
         XCTAssertEqual(model.trips.count,4)
     }
 
+    // NEXT
+    func test_next_oldest() throws {
+        let oldDate = model.formatter.date(from: "2020-01-01")
+        guard let nextTrip = model.trips.next(date: oldDate) else {
+            XCTFail("Trip.next not returned")
+            return
+        }
+        let resultString = model.formatter.string(from: nextTrip.date)
+        XCTAssertEqual(resultString, "2022-07-26")
+    }
+
+    func test_next_middle() throws {
+        let oldDate = model.formatter.date(from: "2022-09-23")
+        guard let nextTrip = model.trips.next(date: oldDate) else {
+            XCTFail("Trip.next not returned")
+            return
+        }
+        let resultString = model.formatter.string(from: nextTrip.date)
+        XCTAssertEqual(resultString, "2022-10-31")
+    }
+
+    func test_next_most_recent() throws {
+        let oldDate = model.formatter.date(from: "2023-02-04")
+        guard let nextTrip = model.trips.next(date: oldDate) else {
+            XCTFail("Trip.next not returned")
+            return
+        }
+        let resultString = model.formatter.string(from: nextTrip.date)
+        XCTAssertEqual(resultString, "2023-06-24")
+    }
+
 }
